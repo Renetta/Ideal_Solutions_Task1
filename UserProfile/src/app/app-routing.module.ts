76a894a12e556954a9ea09e023a7ec63466bddee
Renetta Romeo <renetta92@gmail.com>
2023-08-04 13:12:08 +0300

    Setup Angular Routing
    setup angular routing and created 3 components
    1.userprofile component
    2. address component
    3.company component

--- UserProfile/src/app/app-routing.module.ts
+++ UserProfile/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserprofileComponent } from './components/userprofile/userprofile.component';
+import { AddressComponent } from './components/address/address.component';
+import { CompanyComponent } from './components/company/company.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/user"},
+  { path: "user", component: UserprofileComponent},
+  { path: 'address', component: AddressComponent},
+  { path: 'company', component: CompanyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- UserProfile/src/app/app.module.ts
+++ UserProfile/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserprofileComponent } from './components/userprofile/userprofile.component';
+import { AddressComponent } from './components/address/address.component';
+import { CompanyComponent } from './components/company/company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserprofileComponent,
+    AddressComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
